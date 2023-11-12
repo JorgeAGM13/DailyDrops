69ed406b0b56b6daab43882ae376d87d5ea4c5f8
Jorge Godínez <jorge@gm13.me>
2023-11-12 11:45:58 -0600

    Initial Commit

---
+++ DailyDrops/DailyDropsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDropsApp.swift
+//  DailyDrops
+//
+//  Created by Jorge Alberto Godínez Meza on 12/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct DailyDropsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
